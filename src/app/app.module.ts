48c7c34464f81ab3c86de880f54c6ef76a6a5d95
Catalin Patrut <cpatrut@outlook.com>
2022-11-03 22:53:07 +0100

    adding angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { NgxNotifireModule, NotificationService } from 'ngx-notifire';
 import { ToastDefaults } from 'projects/ngx-notifire/src/lib/defaults/toast-defaults';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, NgxNotifireModule],
+  imports: [BrowserModule, FormsModule, NgxNotifireModule, BrowserAnimationsModule],
   providers: [
     { provide: 'NotifireConfig', useValue: ToastDefaults },
     NotificationService,
