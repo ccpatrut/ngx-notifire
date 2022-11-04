dc30f381efbe1886d29ee984880ca3695214d346
Catalin Patrut <cpatrut@outlook.com>
2022-11-04 21:13:09 +0100

    inputs unstyles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { throws } from 'assert';
 import { NotificationService } from 'ngx-notifire';
 import { BehaviorSubject, Subject } from 'rxjs';
 
@@ -8,14 +10,38 @@ import { BehaviorSubject, Subject } from 'rxjs';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent implements OnInit {
-  style = 'material';
-  subj = new BehaviorSubject<string>('hello2');
-  constructor(readonly notificationService: NotificationService) {}
-  ngOnInit(): void {
-    this.notificationService.emitter.subscribe((s) => console.log(s));
-  }
+  snotifyForm: FormGroup;
 
-  onSuccess() {
-    this.notificationService.success('hi', 'we do it here ');
+  constructor(
+    private readonly fb: FormBuilder,
+    readonly notificationService: NotificationService
+  ) {
+    this.snotifyForm = this.fb.group({
+      toastData: this.fb.group({
+        title: ['Notification title!', [Validators.required]],
+        body: ['Lorem ipsum dolor sit amet!', [Validators.required]],
+      }),
+      toastFunctionalConfig: this.fb.group({
+        titleMaxLength: ['15', [Validators.required]],
+        boydMaxLength: ['78', [Validators.required]],
+        timeout: ['3000', [Validators.required]],
+        backdrop: ['-1', [Validators.required]],
+      }),
+      toastVisualConfig: this.fb.group({
+        isShowProgressBar: [true],
+        isCloseOnClick: [true],
+        isPauseOnHover: [true],
+        isNewItemsOnTop: [true],
+        isFilterDuplicates: [false],
+      }),
+      notificationConf: this.fb.group({
+        maxOnScreen: [6, [Validators.required]],
+        maxAtPosition: [4, [Validators.required]],
+      }),
+      toastStyle: ['', [Validators.required]],
+    });
   }
+  ngOnInit(): void {}
+
+  onSuccess() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,32 @@
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgxNotifireModule, NotificationService } from 'ngx-notifire';
 import { ToastDefaults } from 'projects/ngx-notifire/src/lib/defaults/toast-defaults';
 
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDividerModule } from '@angular/material/divider';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, FormsModule, NgxNotifireModule, BrowserAnimationsModule],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule,
+    NgxNotifireModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatSlideToggleModule,
+    MatFormFieldModule,
+    MatDividerModule,
+  ],
   providers: [
     { provide: 'NotifireConfig', useValue: ToastDefaults },
     NotificationService,
